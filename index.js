c19bec8a06ee4569fd58bfa7df6776f21fea1a8c
Tucker Steil <tuckersteil@gmail.com>
2022-04-14 11:43:48 -0400

    commit tree 24acee9d5c3a988dabb3c92583ae6b39599d81c2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+let newThing = document.querySelector('p')
+
+document.addEventListener("DOMContentLoaded", thing)
+
+function thing(){
+    newThing = document.querySelector('p')
+    newThing.textContent = "This is really cool!"
+}
+
